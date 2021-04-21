e8a647cfef2cd8b21e186938ebc7b1fefdf6a323
Vin Tekiel <vin.tekiel@gmail.com>
2021-04-20 20:21:07 -0700

    initial commit

---
+++ init.lua
@@ -0,0 +1,20 @@
+require('plugins')
+require('colorscheme')
+require('keymappings')
+require('settings')
+
+-- Plugins
+require('nv-compe')
+require('nv-colorizer')
+require('nv-galaxyline')
+require('nv-tree')
+require('nv-galaxyline')
+require('nv-bufferline')
+require('nv-treesitter')
+require('nv-gitsigns')
+require('nv-autopairs')
+require('nv-neogit')
+
+-- LSP
+require('lsp.lua-ls')
+vim.lsp.set_log_level 'debug'
